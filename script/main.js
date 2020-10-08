bbe1a84468cc88c088deabee3933ce185a0cf2c3
ilanprais <38957704+ilanprais@users.noreply.github.com>
2020-10-08 16:15:11 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -5,6 +5,8 @@ var sizeSlider = document.getElementById("sizeBtn");
 var arrayBtn = document.getElementById("arrayBtn");
 var flipBtn = document.getElementById("flipBtn");
 var complexityText = document.getElementById("complexity");
+var wcomplexityText = document.getElementById("wcomplexity");
+var scomplexityText = document.getElementById("scomplexity");
 var comparisonsText = document.getElementById("comparisons");
 var barDiv = document.querySelector(".bars");
 var running = false;
@@ -15,23 +17,24 @@ main();
 function main(){
 
     var run = runBubbleSort;
-    var stats = "O(n<sup>2</sup>)";
+    var stats = ["O(n" + "2".sup() + ")", "O(n" + "2".sup() + ")", "O(1)"];
 
     algorithms ={
-        0: ["Bubble Sort", runBubbleSort, "O(n" + "2".sup() + ")"],
-        1: ["Insertion Sort", runInsertionSort, "O(n" + "2".sup() + ")"],
-        2: ["Selection Sort", runSelectionSort, "O(n" + "2".sup() + ")"],
-        3: ["Merge Sort", runMergeSort, "O(nlog(n))"],
-        4: ["Quick Sort", runQuickSort, "O(nlog(n))"],
-        5: ["Heap Sort", runHeapSort, "O(nlog(n))"],
+        //index: name, algorithm function, average cmplx, worst cmplx, space cmplx
+        0: ["Bubble Sort", runBubbleSort, "O(n" + "2".sup() + ")", "O(n" + "2".sup() + ")", "O(1)"],
+        1: ["Insertion Sort", runInsertionSort, "O(n" + "2".sup() + ")", "O(n" + "2".sup() + ")", "O(1)"],
+        2: ["Selection Sort", runSelectionSort, "O(n" + "2".sup() + ")", "O(n" + "2".sup() + ")", "O(1)"],
+        3: ["Merge Sort", runMergeSort, "O(nlog(n))", "O(nlog(n))", "O(n)"],
+        4: ["Quick Sort", runQuickSort, "O(nlog(n))", "O(n)","O(nlog(n))"],
+        5: ["Heap Sort", runHeapSort, "O(nlog(n))", "O(nlog(n))", "O(1)"]
     };
 
     initArray(arraySize);
 
-    for(let i = 0; i < 5; ++i){
+    for(let i = 0; i < 6; ++i){
         algoBtns[i].addEventListener("change", function(){
             run = algorithms[i][1];
-            updateStats(algorithms[i][2]);
+            updateStats([algorithms[i][2], algorithms[i][3], algorithms[i][4]]);
         });
     }
 
@@ -72,10 +75,10 @@ function initArray(amount){
 function flipArray(){
     var bars = document.getElementsByClassName("bar");
     console.log(bars);
-    for(let i = 0; i < arraySize/2 ; ++i){
+    for(let i = 0; i < bars.length/2 ; ++i){
        var temp = bars[i].style.marginLeft;
-       bars[i].style.marginLeft = bars[arraySize - i - 1].style.marginLeft;
-       bars[arraySize - i - 1].style.marginLeft = temp;
+       bars[i].style.marginLeft = bars[bars.length - i - 1].style.marginLeft;
+       bars[bars.length - i - 1].style.marginLeft = temp;
     } 
 }
 
@@ -101,7 +104,9 @@ function runAnimation(run){
 }
 
 function updateStats(cmplx){
-    complexityText.innerHTML= "Average Time Complexity: " + cmplx;
+    complexityText.innerHTML = "Average Time Complexity: " + cmplx[0];
+    wcomplexityText.innerHTML = "Worst Case Time Complexity: " + cmplx[1];
+    scomplexityText.innerHTML = "Space Complexity: " + cmplx[2];
 }
 
 function disableButtons(){
